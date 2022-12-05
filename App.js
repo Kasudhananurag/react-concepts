80c2fa9114f4339a596cca7c52f991ff17602346
Anurag Kasudhan <114092146+Kasudhananurag@users.noreply.github.com>
2022-12-05 12:12:13 +0530

    Add files via upload
    props and reusable components in react js, just copy these files into your project and run to see how they work

---
+++ App.js
@@ -0,0 +1,13 @@
+import Props from "./Props";
+
+
+function App() {
+  return (
+    <>
+      <Props title="this is a propss passed message" />
+      <Props title="this is a thus a reusable component" />
+    </>
+  );
+}
+
+export default App;

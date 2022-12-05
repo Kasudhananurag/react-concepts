618e92dffe161b415e2f631d6d7296b19f6729cc
Anurag Kasudhan <114092146+Kasudhananurag@users.noreply.github.com>
2022-12-05 13:29:34 +0530

    Add files via upload
    simple routing using react-router-dom and addressbar

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { createContext } from "react";
-import A from "./A";
-import useInc from "./useInc";
-
-const data = createContext();
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import About from "./About";
+import Home from "./Home";
+import Work from "./Work";
 
 function App() {
   const name = "anurag";
   
   return (
     <>
-      <data.Provider value={name}>
-        <A name="hey there" />
-      </data.Provider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/work" element={<Work/>}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
 
 export default App;
-export {data};
